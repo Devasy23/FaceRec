b863d16297faff0d5a5243f2bb3e1302bce76cca
Devasy Patel <20bce057@nirmauni.ac.in>
2024-03-08 14:06:20 +0000

    Add API documentation for create_new_faceEntry, get_employees, read_employee,...

---
+++ FaceRec/app/main/_init_.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+from FaceRec.app.main.Employee import flk_blueprint
+from FaceRec.app.main.VideoImage import employee_blueprint
+
+app = Flask(__name__, template_folder="../../templates/", static_folder="../../static/")
+
+# To register blueprints of flask
+app.register_blueprint(flk_blueprint)
+app.register_blueprint(employee_blueprint)
+
+
+# function to run server of Flast
+def run_flask():
+    app.run(host="127.0.0.1", port=5000)
